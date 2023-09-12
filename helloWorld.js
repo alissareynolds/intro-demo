e0a3cdab3c555c325336a844a6f1daf2c8298243
Alissa Reynolds <alissa6710@gmail.com>
2023-09-12 17:19:16 -0500

    finished the demo

---
+++ helloWorld.js
@@ -0,0 +1,36 @@
+const firstName = "Alissa"
+
+let age = 19 
+
+console.log(firstName, age)
+
+let height;
+
+let heightInFeet = 5;
+let inches = 4;
+
+
+height = heightInFeet * 12
+height += inches
+
+
+
+//console.log(height)
+
+let isCool = true 
+
+let tempLit = `Hi, my name is ${firstName}! I am ${age} years old and it is ${isCool} that I am cool.`
+
+
+// console.log(tempLit)
+
+let score = 102;
+let score2 = 115;
+
+if (score > score2) {
+    console.log('first score is bigger')
+} else {
+    console.log('second is bigger')
+}
+
+
